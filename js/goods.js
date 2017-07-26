a4dbb44ba3f2b0b421dc3727d498638ac522f9ba
zhangli2513 <zhangli2513@sina.com>
2017-07-26 19:53:42 +0800

    商品详情

--- js/goods.js
+++ js/goods.js
@@ -4,10 +4,10 @@
         var space = 20;
         var colume = 5;
         var width = (1200-space*(colume-1))/colume;
-        this.item = $("<div class='good-box'></div>");
+        this.item = $("<div class='good-box' data-id='"+obj.goods_id+"'></div>");
         var name = $("<p class='good-name'>"+obj.goods_name+"</p>");
         var other = $("<p><img width='"+width+"px' src='"+obj.goods_thumb
-                    +"' alt=''></p><h3>￥"+obj.price+"</h3><p>"+obj.goods_desc
+                    +"' alt=''></p><h3 class='goods-price'>￥"+obj.price+"</h3><p class='goods-desc'>"+obj.goods_desc
                     +"</p>");
         this.item.append(name);
         this.item.append(other);
@@ -18,7 +18,9 @@
             "box-sizing": "border-box",
             float:"left",
             overflow: "hidden",
-            position: "relative"
+            position: "relative",
+            "margin-left":"13px",
+            "margin-top":"13px"
         });
         name.css({
             position: "absolute",
@@ -37,7 +39,8 @@
 		return this;
 	}
 	function Good(url,parm,superView,action){
-		this.loadData(url,parm,superView,action)
+		this.loadData(url,parm,superView,action);
+		this.goodDetail(url);
 	};
 	Good.prototype.loadData=function(url,parm,superView,action){
 		$.get(url,parm,function(result){
@@ -47,10 +50,44 @@
 		}.bind(this));
 	};
 	Good.prototype.showGoodsView=function(Goods,superView,action){
-		console.log(Goods);
 		Goods.forEach(function(data){
+		    // console.log(data)
 			superView.append(new GoodItem(data).click(action).item);
 		});
 	};
+	//商品详情
+    Good.prototype.goodDetail = function (url) {
+        $(document).on('click','.good-box',function () {
+            var detId=$(this).data("id");
+            // console.log(detId);
+            $.get(url,{goods_id:detId},function(result){
+                // console.log(result.data);
+                var obj = '';
+                if (result.data.length==2 || result.data.length==1){
+                    obj = result.data[0];
+                }
+                // var detailes=$("<div><p>商品详情</p><img src='"+obj.goods_thumb+"'></div>");
+                var details = `
+                    <p class="container-header">商品详情</p>
+                    <hr/>
+                    <div class="container-body"><img class="header-photo" src="${obj.goods_thumb}"></div>
+                    <div class="container-head">
+                       <p><big>${obj.goods_name}</big></p>
+                       <h3>￥${obj.price}</h3>
+                       <p>${obj.goods_desc}</p>
+                       <div class="header-box"><button class="header-box-one">-</button><input class="header-box-num" type="text" value="1"></input><button class="header-box-two">+</button></div>
+                       <div class="body-box"><button>立即购买</button><button class="body-box-join">加入购物车</button><div>
+                    </div>
+                    <br/><hr/>`;
+                $(".goods-container").html(details);
+                $(".header-box-one").click(function(){
+                	this.nextSibling.value<=1?1:this.nextSibling.value--;
+                });
+                $(".header-box-two").click(function(){
+                	this.previousSibling.value++;
+                });
+            });
+        });
+    }
 	window.Good=Good;
 })();
--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ function init(){
  });
  //轮播
 new corouselView.Corouse("#left-course",[{imagePath:"img/header/hot1.jpg"},{imagePath:"img/header/hot2.jpg"}],200,400).putSuperView().createControlButton().startTimer(2*1000);
-new corouselView.Corouse("#center-course",[{imagePath:"img/header/photomax1.jpg"},{imagePath:"img/header/photomax2.jpg"}],752,400).putSuperView().startTimer(3000,function(){
+new corouselView.Corouse("#center-course",[{imagePath:"img/header/photomax1.jpg"},{imagePath:"img/header/photomax2.jpg"}],800,400).putSuperView().startTimer(3000,function(){
 	var r=parseInt(Math.random()*256);
 	var g=parseInt(Math.random()*256);
 	var b=parseInt(Math.random()*256);
